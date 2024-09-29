3972bd6cad8fbdde1a9cafb0f8122c063f53c32a
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2024-09-29 20:53:25 +0630

    Rewrote myMethod of Top K FrequentElement Algorithm.

--- Top_K_frequentElement.py
+++ Top_K_frequentElement.py
@@ -6,7 +6,7 @@ class Solution:
     def TopFrequent(self,nums:List[int], k:int) -> List[int]:
     
         # all in one line
-        return nums if len(nums)<=1 else [key for key,value in Counter(nums).most_common(k)]
+        # return nums if len(nums)<=1 else [key for key,value in Counter(nums).most_common(k)]
 
         if len(nums)<= 1:
             return nums
@@ -28,6 +28,29 @@ class Solution:
         # The upper 4 lines of code in 1 line.
         return [key for key,value in hash_map.most_common(k)]
 
+    def myMethod_TopK_frequentElement(self, nums:List[int], k:int)->List[int]:
+        if len(nums)<=1:
+            return nums
+        
+        hash_map = {}
+        for i in range(len(nums)):
+            if nums[i] in hash_map:
+                hash_map[nums[i]]+=1
+            else:
+                hash_map[nums[i]]=1
+
+        print(hash_map)
+        sortedArr = sorted(hash_map.items(),key= lambda item:item[1], reverse=True)
+        print("sorted arr is ",sortedArr)
+
+        resultArr = []
+        for i in range(k):
+            print("element of sorted arr is ",sortedArr[i][1])
+            resultArr.append(sortedArr[i][0])
+
+        return resultArr
+
+
 sol = Solution()
-print(sol.TopFrequent([1,1,1,1,1,1,1,1,1,2,2,4,4,4,4,4,4],2))
-print(sol.TopFrequent([1],1))
+print(sol.myMethod_TopK_frequentElement([1,1,1,1,1,1,1,1,1,2,2,4,4,4,4,4,4],2))
+print(sol.myMethod_TopK_frequentElement([1],1))
