a581532f7f242cdbf5c9ac71fbc10fd0f753586a
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2024-10-05 11:11:11 +0630

    Longest Consecutive Sequence myAlgorithm Compact Version.

--- LongestConsecutive.py
+++ LongestConsecutive.py
@@ -29,10 +29,26 @@ class Solution(object):
             longestLen = length
         return longestLen
     
+    # My Solution, But Compact Version
+    def Longest_ConsecutiveSequence_CompactVer(self, numArr:List[int])->int:
+        if not numArr:
+            return 0
+        numArr = sorted(set(numArr))
+        Length = 1
+        longLength = 1
+        for i in range(len(numArr)-1):
+            if numArr[i]+1 == numArr[i+1]:
+                Length+=1
+            else:
+                longLength = max(Length,longLength)
+                Length =1
+        
+        return max(Length,longLength)
     
 sol = Solution()
+print("Length : ",sol.Longest_ConsecutiveSequence_CompactVer([4,8,1,1,10,7,2,6]))
 print("Length : ",sol.Longest_Consecutive([4,8,1,1,10,7,2,6]))
-print("Length : ",sol.Longest_Consecutive([0,3,7,2,5,8,4,6,0,1]))
-print("Length : ",sol.Longest_Consecutive([2,20,4,10,3,4,5]))
-print("Length : ",sol.Longest_Consecutive([0,3,2,5,4,6,1,1]))
-print("Length : ",sol.Longest_Consecutive([100,4,200,1,3,2]))
+print("Length : ",sol.Longest_ConsecutiveSequence_CompactVer([0,3,7,2,5,8,4,6,0,1]))
+print("Length : ",sol.Longest_ConsecutiveSequence_CompactVer([2,20,4,10,3,4,5]))
+print("Length : ",sol.Longest_ConsecutiveSequence_CompactVer([0,3,2,5,4,6,1,1]))
+print("Length : ",sol.Longest_ConsecutiveSequence_CompactVer([100,4,200,1,3,2]))
