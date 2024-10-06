e2ef41e63436097a48d9e6c235e59b70d5ef97dc
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2024-10-06 22:29:51 +0630

    Learned Valid Sudoku Algorithm.

--- ValidSudoku.py
+++ ValidSudoku.py
@@ -1,59 +1,30 @@
+import collections
 from typing import List
 
 
 class Solution:
     def Valid_Sudoku(self, board: List[List[str]])->bool:
-        status = True
-        colArr = []
-        boxArr = []
-        for rowNum in range(len(board)):
-            
-            for itemsIndex in range(len(board[rowNum])):
-                if board[rowNum][itemsIndex] == "." or 0<int(board[rowNum][itemsIndex])<10:
-                    if board[rowNum][itemsIndex] == ".":
-                        board[rowNum][itemsIndex] = 0
-                    else:
-                        board[rowNum][itemsIndex] = int(board[rowNum][itemsIndex])
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
 
-                    # boxArr.append(board[rowNum][itemsIndex])
-                else:
-                    status = False
-                    print("ret")
-                    return status
-                
-            if sol.gotDuplicate(board[rowNum]):
-                status = False
-                print("gotta return row false by funtion")
-                return status
-
-            colArr.append(board[rowNum][0])
-
-        if sol.gotDuplicate(colArr):
-            status = False
-            print("gotta return col false by funtion")
-            return status
-
-
-        print("column arrary : ",colArr)
-        # print("Box array : ",boxArr)
-            
-        print(board)
-
-        return status
-    
-    def gotDuplicate(self,nums:List[int])->bool:
-        found = set()
-        for j in nums:
-            if j in found and j!=0 :
-                return True
-            else:
-                found.add(j)
-                
-        return False
-
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        print("rows : ", rows)
+        print("cols : ", cols)
+        print("squares : ", squares)
+        return True
 
 sol = Solution()
-print(sol.Valid_Sudoku([["1","2",".",".","3",".",".",".","."],
+print(sol.Valid_Sudoku(
+[["1","2",".",".","3",".",".",".","."],
  ["4",".",".","5",".",".",".",".","."],
  [".","9","8",".",".",".",".",".","3"],
  ["5",".",".",".","6",".",".",".","4"],
